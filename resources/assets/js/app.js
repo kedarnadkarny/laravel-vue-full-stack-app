c25eb54b5d8fafa1f1a0da4d927a09475bd39a79
Kedar Nadkarny <kedar.nadkarny@gmail.com>
2018-03-05 15:20:49 -0600

    v1.0.1

--- resources/assets/js/app.js
+++ resources/assets/js/app.js
@@ -15,7 +15,8 @@ window.Vue = require('vue');
  * or customize the JavaScript scaffolding to fit your unique needs.
  */
 
-Vue.component('example', require('./components/Example.vue'));
+Vue.component('navbar', require('./components/Navbar.vue'));
+Vue.component('articles', require('./components/Articles.vue'));
 
 const app = new Vue({
     el: '#app'
